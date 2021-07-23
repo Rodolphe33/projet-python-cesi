79dc416607b173a84c5b0ffd4c7d468a90ab8232
theo <t.samson@cobalt-informatique.com>
2021-07-23 14:35:16 +0200

    challenge 3 change and add sql.py

--- cesi-python/challenge3/challenge3.py
+++ cesi-python/challenge3/challenge3.py
@@ -1,13 +1,9 @@
 import requests
 import json
+import sql
 
 response = requests.get("http://app.objco.com:8099/?account=16L1SPQZS3&limit=5")
-# print(response.status_code)
-# print(response.json)
 dico = response.json()
-# print("1=> ", dico[0])
 for i in range(0, len(dico)):
     json_string = json.dumps(dico[i][0], indent=1)
     print("2=> ", json_string)
-    newList = int(dico[i][0]) + ": " + str(dico[i][1])
-    print(newList)
